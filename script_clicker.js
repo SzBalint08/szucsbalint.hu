d54830209e8ad46d3dc6bf6f6958bd0aae436150
SzBalint08 <rosszcsonpeti@gmail.com>
2025-10-15 22:34:23 +0200

    Clicker mentés

--- script_clicker.js
+++ script_clicker.js
@@ -1,8 +1,22 @@
-let numButtonClicks = 0;
+let pts = 0;
+
+// amikor betölt az oldal, próbálja betölteni a mentett pontot
+window.onload = function() {
+  const savedpts = localStorage.getItem("pts");
+  if (savedpts) {
+    pont = parseInt(savedpts);
+  }
+  frissitPont();
+};
+
 function buttonClicked() {
-    numButtonClicks = numButtonClicks + 1;
-    document.getElementById("mainDiv").textContent =
-        "Pontok: " + numButtonClicks;
+  pts++;
+  localStorage.setItem("pts", pts); // mentés minden kattintásnál
+  frissitPont();
+}
+
+function frissitPont() {
+  document.getElementById("mainDiv").innerText = "pts: " + pts;
 }
 function myFunction() {
     var x = document.getElementById("myTopnav");
