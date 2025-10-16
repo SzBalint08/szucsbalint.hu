a3d5974c62cfa19cf49601802f0384931d0dc53a
SzBalint08 <rosszcsonpeti@gmail.com>
2025-10-16 13:37:09 +0200

    Danish language added

---
+++ script_DK_clicker.js
@@ -0,0 +1,28 @@
+let pts = 0;
+
+// amikor betölt az oldal, próbálja betölteni a mentett pontot
+window.onload = function() {
+  const savedpts = localStorage.getItem("pts");
+  if (savedpts) {
+    pts = parseInt(savedpts);
+  }
+};
+
+function buttonClicked() {
+  pts++;
+  localStorage.setItem("pts", pts); // mentés minden kattintásnál
+  frissitPont();
+}
+
+function frissitPont() {
+  document.getElementById("mainDiv").innerText = "Point: " + pts;
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
